23b900c64ed7d500036c480f629d558f6f94b0f9
balanx <tobalanx@qq.com>
2023-07-12 23:05:45 +0800

    add export_comb

--- fsmlog.py
+++ fsmlog.py
@@ -120,7 +120,8 @@ def vlog_vars() :
     for k,v in src['var'].items() :
         if v[1] == 'wire' :
             txt += 'wire ' + get_range_str(v[0]) + '  ' + k + ' ;\n'
-            txt += 'assign  ' + k + ' = ' + v[2] + ' ;\n'
+            if isinstance(v[2], str) :
+                txt += 'assign  ' + k + ' = ' + v[2] + ' ;\n'
         else :
             txt += 'reg  ' + get_range_str(v[0]) + '  ' + k + ' ;\n'
 
@@ -200,73 +201,138 @@ def vlog_always_2nd() :
     return txt
 
 
-def vlog_export(x) :
+def rst_val(x) :
+    return (str(x) + '\'d0') if (isinstance(x, int)) else ('{' + x + '{1\'b0}}')
 
-    y = ''
+
+def gv_export(x) :
+
     for k,v in x :
-        if  'wire' in v : continue
-
-        y += '\n'
-        if ('trig' in v) or ('hold' not in v) :
-            y += config['tab']*4 + k + ' <= ' + str(v[0]) + '\'d0 ;\n'
 
         i = 0
         for j in v :
             if not isinstance(j, dict) : continue
 
-            y += config['tab']*4
-            if i > 0 : y += 'else '
-
             s = list(j.keys())[0]
             d = list(j.values())[0]
             if '+' in s :
                 config['edge'][s] += '//' + k + '=' + str(d)
+            else :
+                config['node'][s] += '\\n' + k + '=' + str(d)
+
+            i += 1
+
+
+def vlog_export(x) :
+
+    txt = ''
+    for k,v in x :
+        if  'wire' in v : continue
+
+        txt += '\n'
+        if ('trig' in v) or ('hold' not in v) :
+            txt += config['tab']*4 + k + ' <= ' + rst_val(v[0]) + ' ;\n'
+
+        i = 0
+        for j in v :
+            if not isinstance(j, dict) : continue
+
+            txt += config['tab']*4
+            if i > 0 : txt += 'else '
+
+            s = list(j.keys())[0]
+            d = list(j.values())[0]
+            if '+' in s :
                 s = s.split('+')
                 cond = config['state_name'] + '==' + s[0] + ' && ' + config['next_name'] + '==' + s[1]
             else :
-                config['node'][s] += '\\n' + k + '=' + str(d)
                 cond = config[ 'next_name'] + '==' + s
 
-            y += 'if (' + cond + ')' + config['tab']*2 + k + ' <= ' + str(d) + ' ;\n'
+            txt += 'if (' + cond + ')' + config['tab']*2 + k + ' <= ' + str(d) + ' ;\n'
             i += 1
 
-    return y
+    return txt
+
+
+def vlog_export_comb(x, mode=True) : # True is wire, False not
+
+    txt = ''
+    for k,v in x :
+        if  'wire' not in v : continue
+
+        i = 0
+        for j in v :
+            if not isinstance(j, dict) : continue
+
+            if i > 0 :
+                txt += config['tab']*4 + 'else '
+            else :
+                txt += '\n' + config['tab']*4 + k + ' = ' + rst_val(v[0]) + ' ;\n' + config['tab']*4
+
+            s = list(j.keys())[0]
+            d = list(j.values())[0]
+            if '+' in s :
+                s = s.split('+')
+                cond = config['state_name'] + '==' + s[0] + ' && ' + config['next_name'] + '==' + s[1]
+            else :
+                cond = config[ 'next_name'] + '==' + s
+
+            txt += 'if (' + cond + ')' + config['tab']*2 + k + ' = ' + str(d) + ' ;\n'
+            i += 1
+
+    return txt
 
 
 def vlog_always_3rd() :
+    export  = vlog_export(src['var'].items() )
+    export += vlog_export(src['output'].items() )
+    if not export : return export
+
     txt  = '\nalways @(posedge ' + config['clock']
 
     if not config['reset_mode'] :
         txt += ', ' + ('posedge ' if config['reset_edge'] else 'negedge ') + config['reset_name']
 
-    txt += ')\n' + config['tab']*2 + 'if (' + ('' if config['reset_edge'] else '!')
+    txt += ') begin\n' + config['tab']*2 + 'if (' + ('' if config['reset_edge'] else '!')
     txt += config['reset_name'] + ') begin\n'
 
     for k,v in src['var'].items() :
         if v[1] != 'wire' :
-            w = (str(v[0]) + '\'d0') if (isinstance(v[0], int)) else ('{' + v[0] + '{1\'b0}}')
-            txt += config['tab']*4 + k + ' <= ' + w + ' ;\n'
+            txt += config['tab']*4 + k + ' <= ' + rst_val(v[0]) + ' ;\n'
 
     for k,v in src['output'].items() :
         if v[1] != 'wire' :
-            w = (str(v[0]) + '\'d0') if (isinstance(v[0], int)) else ('{' + v[0] + '{1\'b0}}')
-            txt += config['tab']*4 + k + ' <= ' + w + ' ;\n'
+            txt += config['tab']*4 + k + ' <= ' + rst_val(v[0]) + ' ;\n'
 
     txt += config['tab']*2 + 'end\n'
     txt += config['tab']*2 + 'else begin'
 
-
-    txt += vlog_export(src['var'].items() )
-    txt += vlog_export(src['output'].items() )
-
+    txt += export
 
     txt += config['tab']*2 + 'end\n'
-    txt += '\nendmodule // fsmlog\n'
+    txt += '\nend\n'
+
+    return txt
+
+
+def vlog_always_4th() :
+
+    txt  = vlog_export_comb(src['var'].items() )
+    txt += vlog_export_comb(src['output'].items() )
+
+    if txt :
+        txt  = '\nalways @(*) begin' + txt + 'end\n'
+    else :
+        txt  = '\nendmodule // fsmlog\n'
 
     return txt
 
 
 def dot_build() :
+
+    gv_export(src['var'].items() )
+    gv_export(src['output'].items() )
+
     f  = 'digraph fsmlog {\n'
     f += 'rankdir=LR\n'  # LR, UD
     f += 'size="8,5"\n'  # 20,12 / 8,5
@@ -308,6 +374,7 @@ if __name__ == '__main__' :
     txt += vlog_vars()
     txt += vlog_always_2nd()
     txt += vlog_always_3rd()
+    txt += vlog_always_4th()
     print(txt)
 
     txt = dot_build()
